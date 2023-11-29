81aaad626bcaa85e2be1949fb6c965fe377e76c6
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-29 12:39:59 -0600

    Add global usings for turret

---
+++ LethalAPI.Monsters/Usings.cs
@@ -0,0 +1,9 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Usings.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+#pragma warning disable SA1200 // usings should appear in namespaces
+global using GameTurret = Turret;
+global using Turret = LethalAPI.Monsters.Hazards.Turret;
