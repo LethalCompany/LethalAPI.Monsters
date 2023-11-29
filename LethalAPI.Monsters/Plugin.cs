aecfc9cf0ea2a74364672cd5c9a2541010cbd45b
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-28 23:58:24 -0600

    Move plugin class into new proj file. Also use standardized plugin format.

---
+++ LethalAPI.Monsters/Plugin.cs
@@ -0,0 +1,36 @@
+﻿namespace LethalAPI.Monsters;
+
+using System;
+using LethalAPI.Core;
+
+/// <inheritdoc />
+public sealed class Plugin : LethalAPI.Core.Features.Plugin<Config>
+{
+    /// <summary>
+    /// Gets the main <see cref="Plugin"/> instance.
+    /// </summary>
+    public static Plugin Instance { get; private set; } = null!;
+
+    /// <inheritdoc />
+    public override string Name => "LethalAPI-Monsters";
+
+    /// <inheritdoc />
+    public override string Description => "Contains Monsters & Bestiary APIs.";
+
+    /// <inheritdoc />
+    public override string Author => "Lethal API Modding Community";
+
+    /// <inheritdoc />
+    public override Version Version => new Version(0, 0, 01);
+
+    /// <inheritdoc />
+    public override void OnEnabled()
+    {
+        if (!this.Config.IsEnabled)
+        {
+            return;
+        }
+
+        Log.Info($"{this.Name} is being loaded...");
+    }
+}
