0c13dd1bc9c93ee7530b92e67362a4beb3e11567
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-28 23:58:39 -0600

    Add config.

---
+++ LethalAPI.Monsters/Config.cs
@@ -0,0 +1,15 @@
+﻿namespace LethalAPI.Monsters;
+
+using System.ComponentModel;
+
+/// <inheritdoc />
+public sealed class Config : LethalAPI.Core.Interfaces.IConfig
+{
+    /// <inheritdoc />
+    [Description("Indicates whether or not the plugin should be loaded.")]
+    public bool IsEnabled { get; set; }
+
+    /// <inheritdoc />
+    [Description("Indicates whether or not debug logs should be shown.")]
+    public bool Debug { get; set; }
+}
