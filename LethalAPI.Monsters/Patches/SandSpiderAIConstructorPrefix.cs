791faeb328501b0b435a65a53575938c0ffa3f86
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-29 12:39:47 -0600

    Add patches.

---
+++ LethalAPI.Monsters/Patches/SandSpiderAIConstructorPrefix.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SandSpiderAIConstructorPrefix.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Monsters.Patches;
+
+using Core;
+using HarmonyLib;
+using Monsters.Hazards;
+
+/// <summary>
+/// Gets the instance of the SandSpiderAI.
+/// </summary>
+[HarmonyPatch(typeof(SandSpiderAI), MethodType.Constructor)]
+internal static class SandSpiderAIConstructorPrefix
+{
+    [HarmonyPrefix]
+    private static void Prefix(SandSpiderAI __instance)
+    {
+        Log.Debug("Got SandSpiderAI Instance.");
+        Web.SpiderAI = __instance;
+        Web.WebPrefab = __instance.webTrapPrefab;
+    }
+}
